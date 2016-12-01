a8cc697894d9aa1d699ee8eb6fe36a2bd2db511e
doit05 <768068275@qq.com>
2016-12-01 21:39:46 +0800

    add

---
+++ apue/cmake-build-debug/CMakeFiles/apue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apue.dir/main.cpp.o"
+  "apue.pdb"
+  "apue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
