2f989926717c6639cc697afefe492795bcdc4ec8
doit05 <768068275@qq.com>
2016-12-03 11:38:05 +0800

    add NULL

--- helloc/dockerc/clone.c
+++ helloc/dockerc/clone.c
@@ -5,13 +5,13 @@
 static char child_stack[STACK_SIZE];
 char * const child_args[] = {
         "/bin/bash",
-        "NULL"
+        NULL
 };
 
 int child_main(void * args){
     printf("in child proces %d \n", getpid());
     execv(child_args[0], child_args);
-    return 1;
+    return 2;
 
 }
 
@@ -20,7 +20,7 @@ int main(){
     int child_pid = clone(child_main,child_stack + STACK_SIZE, SIGCHLD, NULL);
     int *ret = 0;
     int wait = waitpid(child_pid, ret, 0);
-    printf("已经退出 wait : %d, ret : %d\n", wait, ret);
+    printf("已经退出 wait : %d, ret : %d\n", wait, *ret);
     return 0;
 
 }
